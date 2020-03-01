8787ded7ece0805df04e306872ad8be2faa21ef4
Sam Ho <sam.ho@bodytrak.co>
2020-03-01 00:12:19 +0000

    test

--- heartRateSensor.js
+++ heartRateSensor.js
@@ -8,7 +8,11 @@
       this._characteristics = new Map();
     }
     connect() {
-      return navigator.bluetooth.requestDevice({filters:[{services:[ 'heart_rate' ]}]})
+      // return navigator.bluetooth.requestDevice({filters:[{services:[ 'heart_rate' ]}]})
+      return navigator.bluetooth.requestDevice({filters:[{
+        namePrefix: 'BodyTrak'}],
+        optionalServices: ['battery_service']
+      })
       .then(device => {
         this.device = device;
         return device.gatt.connect();
